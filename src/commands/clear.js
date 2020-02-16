e2e44d14f091cce64e56d4663fad0733d82480f0
Abeous <abeouss@gmail.com>
2020-02-16 15:02:25 -0500

    fix legacy code bug

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 
 module.exports = {
-  clear: (username, ws) => { try {
+  clear: (username, chatTools) => { try {
       fs.writeFileSync(`./logs/${username}.txt`, '')
-      ws.send(`PRIVMSG {"nick":"${username}", "data":"Your mentions log has been cleared."}`)
+      chatTools.sendPrivateMessage(username, "Your mentions log has been cleared.")
     } catch (e) { console.error(e) }
   }
 }
