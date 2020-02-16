0bdc07c7eb09b9c15e31eef4fccff6498c94db9e
Abeous <abeouss@gmail.com>
2020-02-16 14:59:54 -0500

    fix old log folder bug

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 module.exports = {
   clear: (username, ws) => { try {
-      fs.writeFileSync(`./users/${username}.txt`, '')
+      fs.writeFileSync(`./logs/${username}.txt`, '')
       ws.send(`PRIVMSG {"nick":"${username}", "data":"Your mentions log has been cleared."}`)
     } catch (e) { console.error(e) }
   }
